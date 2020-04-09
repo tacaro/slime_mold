40604ae8159babd64224670f6d12fddd577896bc
tacaro <tristan.caro@colorado.edu>
2020-04-09 15:13:24 -0600

    having trouble with chemical subset not updating

--- model.py
+++ model.py
@@ -3,55 +3,80 @@ from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
 
+
+'''Here we define functions applied at the model-wide scale'''
 def compute_total_chemical(model):
-    agent_chems = [agent.chem for agent in model.schedule.agents if agent.chem]
+    agent_chems = [agent.chem for agent in model.schedule.agents if isinstance(agent, ChemAgent)]
     return sum(agent_chems)
 
+
+'''Here we define the two kinds of agents the model accepts: slime and chem'''
 class ChemAgent(Agent):
-    '''A ubiquitous agent representing chemical concentration'''
+    '''
+    An agent representing chemical concentration. This will be spawned in
+    every gridspace and hold a float value representing chemical concentration.
+    '''
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.chem = 1
-
-    def evaporate(self):
-        self.chem -= 0.1
 
     def step(self):
         if self.chem > 0:
             self.chem -= 0.05
 
+
 class SlimeAgent(Agent):
-    '''An agent that excretes chemical, senses, and moves towards higher chemical
-    concentration.'''
+    '''
+    An agent that excretes chemical, senses, and moves towards higher chemical
+    concentration. This agent represents a single slime mold cell.
+    '''
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.chem = False # including this so the model knows that this is
-        # not a chem agent
-        self.is_slime = True # bool denoting that this is a slime
+        self.chem = 0
 
     def secrete(self):
+        # for neighbor in self.model.grid.neighbor_iter(self.pos)
         pass
 
+    def step(self):
+        # self.move()
+        self.secrete()
 
 
+
+'''Below we define the model parameters'''
 
 class SlimeModel(Model):
-    '''A model with some number of slime and ubiquitous chemical'''
-    def __init__(self, N, width, height):
-        self.num_agents = N # number of slime agents to add
+    '''
+    A model with some number of slime and ubiquitous chemical
+    Args:
+        pop: number of slime cells to add to the model
+        width: grid width
+        height: grid height
+    '''
+    def __init__(self, pop, width, height):
+        self.N = pop # number of slime agents to spawn
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
         self.running = True
 
         # Create agents
-        for i in range(width*height): # for every cell in the grid
-            a = ChemAgent(i, self) # generate a chem agent
-            self.schedule.add(a) # add to the schedule
 
-            # Add agent to every grid cell
-            for row in range(height): # for every row
-                for col in range(width): # for every column
-                    self.grid.place_agent(a, (row, col)) # place a chem agent
+        # Add chem agent to every grid cell
+        for row in range(height): # for every row
+            for col in range(width): # for every column
+                id = ((row*10) + col) # unique_id is row,col as 2 digit number
+                a = ChemAgent(id, self) # generate a chem agent
+                self.schedule.add(a) # add to the schedule
+                self.grid.place_agent(a, (row, col)) # place a chem agent
+
+        # Add slime agent randomly, population specified by N
+        for i in range(self.N):
+            slm = SlimeAgent(i, self)
+            self.schedule.add(slm)
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(slm, (x, y))
 
         # Initialize datacollector
         self.datacollector = DataCollector(
