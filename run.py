1d0eef720fd1ba32b5a727fbbae2135c3ac9e0a1
tacaro <tristan.caro@colorado.edu>
2020-04-09 15:13:35 -0600

    having trouble with chemical subset not updating

--- run.py
+++ run.py
@@ -4,19 +4,36 @@ from mesa.visualization.ModularVisualization import ModularServer
 from model import * # our model module
 
 # We need to provide that takes an agent, returns a portrayal object
+'''Universal Parameters'''
+width = 10 # width of grid
+height = 10 # height of grid
+slime_population = 25 # how many slime cells to add
+arena_size = 500 # grid cell width in pixels
 
 def agent_portrayal(agent):
-    if agent.chem:
+    if isinstance(agent, ChemAgent):
+        if agent.chem > 0:
+            portrayal = {"Shape": "circle",
+                         "Color": "blue",
+                         "Filled": "true",
+                         "Layer": 0,
+                         "r": agent.chem}
+        else:
+            portrayal = {"Shape": "circle",
+                         "Color": "blue",
+                         "Filled": "true",
+                         "Layer": 0,
+                         "r": 0}
+    if isinstance(agent, SlimeAgent):
         portrayal = {"Shape": "circle",
                      "Color": "red",
                      "Filled": "true",
-                     "Layer": 0,
-                     "r": agent.chem}
-
+                     "Layer": 1,
+                     "r": 0.33}
     return portrayal
 
 # Instantiate canvas grid with width and height in cells/pixels
-grid = CanvasGrid(agent_portrayal, 20, 20, 500, 500)
+grid = CanvasGrid(agent_portrayal, width, height, arena_size, arena_size)
 
 # Create a chart for the total amount of chemical
 chart = ChartModule([{"Label": "Total_Chem",
@@ -27,6 +44,7 @@ chart = ChartModule([{"Label": "Total_Chem",
 server = ModularServer(SlimeModel, # the model to feed in
                        [grid, chart], # the list of objects to include in the viz
                        "Slime Model", # title
-                       {"N":100, "width":20, "height":20}) # arguments for the model
+                       {"pop":slime_population, "width":width, "height":height}) # arguments for the model
+
 server.port = 8251 # the default port
 server.launch()
