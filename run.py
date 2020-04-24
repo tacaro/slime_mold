1de915bd89e77ee1bbad4703c5be319f2a0b3abc
tacaro <tristan.caro@colorado.edu>
2020-04-24 09:52:47 -0600

    changed params

--- run.py
+++ run.py
@@ -6,9 +6,9 @@ import argparse
 
 # We need to provide that takes an agent, returns a portrayal object
 '''Universal Parameters'''
-width = 50 # width of grid
-height = 50 # height of grid
-slime_population = 100 # how many slime cells to add
+width = 20 # width of grid
+height = 20 # height of grid
+slime_population = 20 # how many slime cells to add
 arena_size = 750 # grid cell width in pixels
 
 def agent_portrayal(agent):
